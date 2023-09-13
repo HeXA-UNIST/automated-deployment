f8074d99cc10d8d561ac840dc6cbb0da6a977ef0
junwha0511 <junwha0511@naver.com>
2023-09-13 13:17:52 +0900

    Log and shutdown by pid

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gunicorn main:app --access-logfile `pwd`/access.log --error-logfile `pwd`/error.log -b 0.0.0.0:5555 -w 4 --timeout=10 -k gevent --daemon
+kill -9 $(cat app.pid)
+gunicorn main:app -p app.pid --access-logfile `pwd`/access.log --error-logfile `pwd`/error.log -b 0.0.0.0:5555 -w 4 --timeout=10 -k gevent --daemon
